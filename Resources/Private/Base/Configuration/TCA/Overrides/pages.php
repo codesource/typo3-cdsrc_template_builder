662169ef115661e8b4874a7ee52eb637b3e5c7a2
Matthias Toscanelli <m.toscanelli@code-source.ch>
2017-10-27 13:54:24 +0200

    [TASK] Move TCA modification from ext_tables to TCA configurations files

--- Resources/Private/Base/Configuration/TCA/Overrides/pages.php
+++ Resources/Private/Base/Configuration/TCA/Overrides/pages.php
@@ -1,19 +1,16 @@
 <?php
-
-if (!defined('TYPO3_MODE'))
-    die('Access denied.');
-
-// Load default extension template
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Default Template');
+/**
+ * @copyright Copyright (c) 2017 Code-Source
+ */
 
 // Load dynamically PageTS
-$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
+$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('{|EXTENSION_KEY|}');
 $pagesTS = \TYPO3\CMS\Core\Utility\GeneralUtility::getFilesInDir($extensionPath . 'Configuration/PageTS', 'txt', TRUE, '', '^_.*$');
 foreach ($pagesTS as $file) {
     $name = ucwords(preg_replace('/[^a-z0-9]/i', ' ', pathinfo($file, PATHINFO_FILENAME)));
     $path = substr($file, strlen($extensionPath));
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        $_EXTKEY,
+        '{|EXTENSION_KEY|}',
         $path,
         $name . ' configuration'
     );
---
+++ Resources/Private/Base/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Code-Source
+ */
+
+
+
+// Load default extension template
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('{|EXTENSION_KEY|}', 'Configuration/TypoScript', 'Default Template');
